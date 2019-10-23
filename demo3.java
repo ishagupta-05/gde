6dbce7a5ade9a7e575b9d43d244c58a6966ca69c
ishagupta-05 <49872185+ishagupta-05@users.noreply.github.com>
2019-10-23 09:36:06 +0530

    Add files via upload

---
+++ demo3.java
@@ -0,0 +1,15 @@
+public class demo3
+
+{
+public static void main(String[] args) 
+    {
+        E ob =new E();
+        ob.getdata();        
+        ob.display();
+        ob.getdata1();
+        ob.display();        
+        ob.cal();
+
+    }
+    
+}
